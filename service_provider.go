656616470a5c78f1f6877470ea455f4d03ac6880
gtestault <gabriel.testault@logmein.com>
2020-01-16 13:30:47 +0100

    change default name id format

--- service_provider.go
+++ service_provider.go
@@ -824,7 +824,7 @@ func (sp *ServiceProvider) nameIDFormat() string {
 	switch sp.AuthnNameIDFormat {
 	case "":
 		// To maintain library back-compat, use "transient" if unset.
-		nameIDFormat = string(TransientNameIDFormat)
+		nameIDFormat = string(UnspecifiedNameIDFormat)
 	case UnspecifiedNameIDFormat:
 		// Spec defines an empty value as "unspecified" so don't set one.
 	default:
